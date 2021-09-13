04b05a86347914a006c8c6e6f49b06124321dbdd
David Coats <david@getsafetycone.com>
2021-09-13 14:09:14 -0700

    Add documentation, set C# language version, and add new project reference dependency.

---
+++ source/R5T.Suebia.Base/Code/Documentation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Suebia
+{
+    /// <summary>
+    /// <see cref="ISecretsDirectoryFilePathProvider"/> and <see cref="ISecretsDirectoryPathProvider"/> services.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
